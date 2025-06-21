192e3f2a2fdcca655ad353c3523737eeeeadf215
Ziming Lei <ziminglei123@gmail.com>
2025-06-20 22:27:55 -0700

    Implemented functions to output the highlighted room

---
+++ backend/room_finder.py
@@ -0,0 +1,93 @@
+"""
+room_finder.py
+
+Helper functions to get highlight the room on the matching PDF.
+"""
+import schedule_parser as sched_parser
+
+from rapidfuzz import process, fuzz
+from rich.console import Console
+import fitz
+
+console = Console()
+
+# ---------- Get the best matching building name --------------
+
+def best_match_building_name(target_name: str, possible_choices: list[str]) -> str:
+    """
+    Use fuzzy matching to find the closest match of the official building name
+    inside the list of extracted text from the PDF.
+
+    Args:
+        target_name (str): The WWU building name (e.g. "Communications Facility").
+        possible_choices (list[str]): Possible building names. 
+
+    Returns:
+        str: Best matched name from the extracted lines, or the original if no good match found.
+    """
+    candidates = [line.strip().lower() for line in possible_choices if line.strip()]
+    target = target_name.strip().lower()
+
+    match = process.extractOne(target, candidates, scorer=fuzz.WRatio)
+    return match[0].title() if match and match[1] >= 70 else target_name
+
+
+# ---------- Highlight the room number on the matched PDF --------------
+
+def highlight_room_on_floorplan(pdf_path: str, room_num: str, output_path: str, padding: float = 16.0, border_width : float = 2.0) -> None:
+    """
+    Searches for a specific room label (text) in a PDF, highlights it with a colored box,
+    and saves a new single-page PDF showing only the page with the match.
+
+    Args:
+        pdf_path (str): Path to the input PDF file to search.
+        room_num (str): Text string to search for (e.g.,"110").
+        output_path (str): Path to save the resulting one-page highlighted PDF.
+        padding (float, optional): Extra padding around the text bounding box (in points). Default is 16.0.
+        border_width (float, optional): Thickness of the red border (in points). Default is 2.0.
+
+    Returns:
+        None: The function saves the PDF to `output_path` and prints success/failure via `console`.
+              If the room label is not found, no file is saved.
+    """
+    doc = fitz.open(pdf_path)
+    for page_num, page in enumerate(doc):
+        text_instances = page.search_for(room_num)
+
+        if text_instances:
+            # copy the matched pdf for highlight
+            out_doc = fitz.open()
+            new_page = out_doc.new_page(width=page.rect.width, height=page.rect.height)
+            new_page.show_pdf_page(page.rect, doc, page_num)
+            # get first matched coordinates
+            coordinates = text_instances[0]
+            x0, y0, x1, y1 = coordinates
+            expanded_rect = fitz.Rect(x0-padding, y0-padding, x1+padding, y1+padding)
+            # set the highlighted square
+            highlight = new_page.add_rect_annot(expanded_rect)
+            highlight.set_colors(stroke=(1, 0, 0), fill=(1, 1, 0))
+            highlight.set_border(width=border_width)
+            highlight.set_opacity(0.7)
+            highlight.update()
+            # save the highlighted pdf to output_path
+            out_doc.save(output_path)
+            console.print(f"[green]✓ Saved[/] [red]room {room_num}[/] [green]highlighted PDF[/]: [cyan]{output_path}[/]")
+            # close for better practice
+            out_doc.close()
+            doc.close()
+            return
+    
+    console.print(f"[red] NO match for[/] [cyan]room {room_num}[/]")
+    doc.close()
+
+
+if __name__ == "__main__":
+    path = "./data/floorplans/CF.pdf"
+    room_num = "015"
+
+    extracted_lines = sched_parser.extract_raw_text_from_pdf(path, page_number=0)
+    prv_name = "Communications facility"
+    building_name = best_match_building_name(prv_name, extracted_lines)
+    console.print(f"\n[bold red]BEST MATCH[/] --> [bold green]{prv_name}[/]: [yellow]{building_name}[/]\n")
+
+
--- backend/schedule_parser.py
+++ backend/schedule_parser.py
@@ -1,9 +1,10 @@
 """
 schedule_parser.py
 
-Get the text from schedule pdf and put it into a JSON file in temporary_data
+Get the text from schedule pdf and highlighted rooms to put it into a JSON file in temporary_data
     - temporary_data: a temporary directory to be deleted for each user for privacy after finish
 """
+import room_finder
 
 import os.path
 import fitz
@@ -20,6 +21,9 @@ console = Console()
 # change below path by user file
 PDF_PATH = "./temporary_data/schedule.pdf"
 OUTPUT_SCHEDULE_JSON = "./temporary_data/schedule_parsed.json"
+BUILDING_MAP_JSON = "./data/building_map.json"
+BUILDING_FLOORPLAN_DIR = "./data/floorplans"
+FLOORPLAN_OUT_DIR = "./temporary_data/floorplans"
 
 # ---------- Extract text from fiven PDF --------------
 
@@ -82,10 +86,13 @@ def parse_schedule_text(lines: str) -> dict:
     day_names_set = set(day_names)
 
     # find every index that is a course‐code line
-    # code_pattern: find the course abbreviation - e.g. "MATH", "CS", "M/CS"
+    # code_pattern: find the course abbreviation - e.g., "MATH", "CS", "M/CS"
     code_pattern = re.compile(r'^[A-Z/]+(?:/[A-Z]+)?\s*\d{3}[A-Z]?\s+\d+$')
     # code_indices: get the index from the list that contain the course code
     code_indices = [i for i, L in enumerate(lines) if code_pattern.match(L)]
+
+    # meeting_date_pattern: from to end date - e.g., 09/27/2023 - 12/15/2023)
+    meeting_date_pattern = re.compile(r'^\d{2}/\d{2}/\d{4}\s*-\s*\d{2}/\d{2}/\d{4}$')
     
     for idx in code_indices:
         # title: the line just above current idx
@@ -95,37 +102,52 @@ def parse_schedule_text(lines: str) -> dict:
         # parse course code (no trailing section number)
         # Ex: "CSCI 447 0" --> "CSCI 447"
         course_code = code_sec.rsplit(None, 1)[0]
-        
-        # lines below:
+        idx = idx + 1
+        # skip credit hours and CRN
         # idx+1 --> credit
         # idx+2 --> CRN
-        # idx+3 --> date (Ex: "01/07/2025 - 03/21/2025")
-        days_line     = lines[idx+4]
-        time_line     = lines[idx+5]
-        location_line = lines[idx+6]
-        instructor    = lines[idx+7]
+        idx += 2
         
-        # split out days (Ex: "Monday, Tuesday, Wednesday, Friday") 
-        # into each element to a list (Ex: ["Monday", "Tuesday", "Wednesday", "Friday"])
-        days = [d.strip() for d in days_line.split(',') if d.strip() in day_names_set]
+        # find the meeting block
+        while idx < len(lines) and meeting_date_pattern.match(lines[idx]):            
+            # idx --> date (Ex: "01/07/2025 - 03/21/2025")
+            days_line     = lines[idx+1]
+            time_line     = lines[idx+2]
+            location_line = lines[idx+3]
         
-        # pull building/room from the last two comma‐parts
-        temp = [p.strip() for p in location_line.split(',')]
-        building = temp[-2]
-        room     = temp[-1]
+            # find instructor name or next meeting date range
+            peek = lines[idx+4].strip()
+            if meeting_date_pattern.match(peek) or code_pattern.match(peek) or not peek:
+                instructor = "Unknown Instructor"
+                move_by = 4
+            else:
+                instructor = peek
+                move_by = 5
+            
+            # skip lines
+            idx += move_by
         
-        # build the entry
-        entry = {
-            'course': f"{course_code} - {title}",
-            'time':   time_line,
-            'building': building,
-            'room':    room, 
-            'instructor' : instructor
-        }
+            # split out days (Ex: "Monday, Tuesday, Wednesday, Friday") 
+            # into each element to a list (Ex: ["Monday", "Tuesday", "Wednesday", "Friday"])
+            days = [d.strip() for d in days_line.split(',') if d.strip() in day_names_set]
+            
+            # pull building/room from the last two comma‐parts
+            temp     = [p.strip() for p in location_line.split(',')]
+            building = temp[-2]
+            room     = temp[-1]
+            
+            # build the entry
+            entry = {
+                'course': f"{course_code} - {title}",
+                'time':   time_line,
+                'building': building,
+                'room':    room, 
+                'instructor' : instructor
+            }
         
-        # append to each day
-        for d in days:
-            schedule[d].append(entry)
+            # append to each day
+            for d in days:
+                schedule[d].append(entry)
 
     # helper to sort by start time
     def get_start_time(entry):
@@ -166,6 +188,91 @@ def print_schedule_weekday(sched: dict) -> None:
             print("\n")
 
 
+# ---------- Create and add the highlighted PDF to schedule dictionary --------------
+
+def load_building_map(path: str) -> dict[str, str]:
+    """
+    Load the JSON file.
+
+    Args:
+        path (str): JSON file path.
+    
+    Returns:
+        JSON file (dict[str, str])
+    """
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def highlight_floorplans(sched: dict) -> None:
+    """
+    Create the corresponding highlighted pdf for each course entry.
+
+    For each entry in the weekly schedule:
+      1. Resolve the raw building name against the official map.
+      2. Skip entries for which no floorplan exists.
+      3. Generate (once per building+room) a one‑page PDF with that room highlighted.
+      4. Cache generated PDFs so that subsequent entries reuse the same file.
+      5. Add a "map_pdf" field to each entry containing the output path (or "" if not found).
+
+    Args:
+        sched (dict): A mapping weekday → list of schedule entries, where each entry is a dict 
+                        with at least "building" and "room" keys.
+    Returns:
+        None: Mutates each entry in-place, setting entry["map_pdf"].
+    """
+    os.makedirs(FLOORPLAN_OUT_DIR, exist_ok=True)
+    # load the JSON from another directory
+    building_map = load_building_map(BUILDING_MAP_JSON)
+    official_names = list(building_map.keys())
+
+    # storage to avoid creating repeated pdf: building_pdf_key --> output_pdf_path
+    cache = {}
+
+    # loop through all entries
+    for entries in sched.values():
+        for entry in entries:
+            raw_name = entry["building"]
+            room = entry["room"]
+
+            # get the building name
+            if raw_name in building_map:
+                match_name = raw_name
+            else:
+                match_name = room_finder.best_match_building_name(raw_name, official_names)
+                console.print(f"[yellow]⚠️  [magenta]NAME CHANGE:[/] '{raw_name}' --> '{match_name}'[/]")
+            
+            key = f"{match_name}_{room}"
+            # reuse existing PDF if already exist
+            if key in cache:
+                entry["map_pdf"] = cache[key]
+                continue
+
+            # get the floor plan PDF name
+            pdf_filename = building_map.get(match_name)
+            if not pdf_filename:
+                console.print(f"[red]NO floorplan PDF for[/] [bold]{raw_name}[/]")
+                entry["map_pdf"] = ""
+                continue
+
+            # compute the input and output file path
+            in_pdf = os.path.join(BUILDING_FLOORPLAN_DIR, pdf_filename)
+            building_abbr = pdf_filename.replace(".pdf", "")
+            out_pdf = os.path.join(
+                FLOORPLAN_OUT_DIR, 
+                f"{building_abbr}_{room}.pdf"
+            )
+
+            # generate the highlighted PDF
+            room_finder.highlight_room_on_floorplan(in_pdf, room, out_pdf)
+
+            if os.path.exists(out_pdf):
+                cache[key] = out_pdf
+                entry["map_pdf"] = out_pdf
+            else:
+                entry["map_pdf"] = ""
+
+
 # ---------- Create a JSON file for schedule --------------
 
 def create_sched_json(sched: dict) -> None:
@@ -192,8 +299,10 @@ def process_parser_for_schedule():
     """
     Run the complete logic for parsing the schedule
         1. Extract text from schedule.pdf
-        2. Parse it to a dictionary: print schedule if needed
-        3. Create a JSON file to store the schedule
+        2. Parse it to a dictionary
+        3. Create the corresponding highlighted room pdf
+        4. Print schedule to terminal
+        5. Create a JSON file to store the schedule
     """
     raw_text = extract_raw_text_from_pdf(PDF_PATH)
     # get the list entries:
@@ -203,8 +312,9 @@ def process_parser_for_schedule():
     """
 
     sched = parse_schedule_text(raw_text)
-    console.print(f"[bold green]✓ Schedule created from:[/bold green] {PDF_PATH}")
+    console.print(f"[bold green]✓ Schedule created from:[/bold green] {PDF_PATH}\n")
     
+    highlight_floorplans(sched)
     print_schedule_weekday(sched)
 
     """
